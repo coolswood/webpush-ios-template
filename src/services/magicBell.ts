cd1452bc6f482bf225331b6e2478534813e9cf68
coolswood <coolswood@yandex.ru>
2024-12-22 21:50:21 +0200

    fi

--- src/services/magicBell.ts
+++ src/services/magicBell.ts
@@ -59,7 +59,7 @@ class MagicBell {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ userId: '2045794073282663', title }),
+      body: JSON.stringify({ userId: "2606506588496713", title }),
     })
     if (!response.ok) {
       throw new Error(`Failed to send custom notification`)
