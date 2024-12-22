892d0879b79ccd04d609e06973b31f87cfa68de1
coolswood <coolswood@yandex.ru>
2024-12-22 22:25:33 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -167,10 +167,10 @@ export default function Home() {
       </main>
 
       <div className="px-4 flex flex-col gap-2">
-      <button onClick={() => sendHello(`🫂 ${partnerType === "wife" ? "Мужу" : "Жене"} требуются объятия!`)} className="btn btn-neutral w-full text-white">🫂 Требуются объятия!</button>
-      <button onClick={() => sendHello(`😔 Приступ грусти y ${partnerType === "wife" ? "мужа" : "жены"}`)} className="btn btn-neutral w-full text-white">😔 Приступ грусти</button>
-      <button onClick={() => sendHello(`😋 ${partnerType === "wife" ? "Муж" : "Жена"} голодна!`)} className="btn btn-neutral w-full text-white">😋 Хочу кушать!</button>
-      <button onClick={() => sendHello(`🔍️ Где же ${partnerType === "wife" ? "муж" : "жена"}?`)} className="btn btn-neutral w-full text-white">🔍️ Где же тыыыы?</button>
+      <button onClick={() => sendHello(`🫂 ${partnerType === "husband" ? "Мужу" : "Жене"} требуются объятия!`)} className="btn btn-neutral w-full text-white">🫂 Требуются объятия!</button>
+      <button onClick={() => sendHello(`😔 Приступ грусти y ${partnerType === "husband" ? "мужа" : "жены"}`)} className="btn btn-neutral w-full text-white">😔 Приступ грусти</button>
+      <button onClick={() => sendHello(`😋 ${partnerType === "husband" ? "Муж" : "Жена"} голодна!`)} className="btn btn-neutral w-full text-white">😋 Хочу кушать!</button>
+      <button onClick={() => sendHello(`🔍️ Где же ${partnerType === "husband" ? "муж" : "жена"}?`)} className="btn btn-neutral w-full text-white">🔍️ Где же тыыыы?</button>
       </div>
     </>
   );
